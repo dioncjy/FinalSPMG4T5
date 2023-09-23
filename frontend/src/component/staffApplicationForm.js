fec9d1871b80eb9f63b6dfeb09ad2329375e62c2
joshuahoe <joshuahoe0131@gmail.com>
2023-09-23 18:42:02 +0800

    SPM-48 added buttons for staff application form

--- frontend/src/component/staffApplicationForm.js
+++ frontend/src/component/staffApplicationForm.js
@@ -1,69 +1,77 @@
 import React from 'react';
+import { Button, UserPlusIcon } from "@material-tailwind/react";
 
-function staffApplicationForm() {
+export default function staffApplicationForm() {
     return (
-        <div className="flex items-center justify-center h-screen">
+        <div className="w-32">
+            <div className="p-6 bg-blue-500">
             <div className="border p-6 rounded-lg">
-                <h3>
-                    Applying for 
-                </h3>
-                <p>
-                    Web Application, Singapore
-                </p>
-                <hr/>
+                <div className="flex flex-col">
+                    <h3>
+                        Applying for 
+                        {/* fill in dynamic data */}
+                    </h3>
+                    <p>
+                        Web Application, Singapore
+                    </p>
+                    <hr/>
 
-                <div className="py-2">
-                    <label for="id">
-                        <strong>Staff ID</strong>
-                    </label>
-                    <br/>
-                    <input type="text" name="id" id="id" className="border w-full rounded-md"></input>
-                </div>
-                <hr/>
+                    <div className="py-2">
+                        <label for="id">
+                            <strong>Staff ID</strong>
+                        </label>
+                        <br/>
+                        <input type="text" name="id" id="id" className="border w-full rounded-md"></input>
+                    </div>
+                    <hr/>
 
-                <div className="py-2">
-                    <label for="name">
-                        <strong>Applicant Name</strong>
-                    </label>
-                    <br/>
-                    <input type="text" name="name" id="name" className="border w-full rounded-md"></input>
-                </div>
-                <hr/>
+                    <div className="py-2">
+                        <label for="name">
+                            <strong>Applicant Name</strong>
+                        </label>
+                        <br/>
+                        <input type="text" name="name" id="name" className="border w-full rounded-md"></input>
+                    </div>
+                    <hr/>
 
-                <div className="py-2">
-                    <label for="dept">
-                        <strong>Department</strong>
-                    </label>
-                    <br/>
-                    <input type="text" name="dept" id="dept" className="border w-full rounded-md"></input>
-                </div>
-                <hr/>
+                    <div className="py-2">
+                        <label for="dept">
+                            <strong>Department</strong>
+                        </label>
+                        <br/>
+                        <input type="text" name="dept" id="dept" className="border w-full rounded-md"></input>
+                    </div>
+                    <hr/>
 
-                <div className="py-2">
-                    <label for="country">
-                        <strong>Country</strong>
-                    </label>
-                    <br/>
-                    <input type="text" name="country" id="country" className="border w-full rounded-md"></input>
-                </div>
-                <hr/>
+                    <div className="py-2">
+                        <label for="country">
+                            <strong>Country</strong>
+                        </label>
+                        <br/>
+                        <input type="text" name="country" id="country" className="border w-full rounded-md"></input>
+                    </div>
+                    <hr/>
 
-                <div className="py-2">
-                    <label for="comments">
-                        <strong>Comments</strong>
-                    </label>
-                    <br/>
-                    <textarea
-                    rows={4}
-                    className="border rounded-md p-2 w-full"
-                    placeholder="Fill in your comments"
-                    />
+                    <div className="py-2">
+                        <label for="comments">
+                            <strong>Comments</strong>
+                        </label>
+                        <br/>
+                        <textarea
+                        rows={4}
+                        className="border rounded-md p-2 w-full"
+                        placeholder="Fill in your comments"
+                        />
+                    </div>
+                    <hr/>
+                    <div className="py-2 flex justify-between">
+                        <Button className="bg-violet-600">Home</Button>
+                        <Button className="bg-violet-600">Submit</Button>
+                    </div>
                 </div>
-                <hr/>
             </div>
-
+            </div>
         </div>
     );
 }
 
-export default staffApplicationForm;
