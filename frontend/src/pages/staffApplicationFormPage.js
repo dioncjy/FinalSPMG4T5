90dbb63f804f83a4cb07eb068a0085a7f4e4de70
joshuahoe <joshuahoe0131@gmail.com>
2023-09-26 14:04:04 +0800

    SPM-48 add header and navbar to SA form

--- frontend/src/pages/staffApplicationFormPage.js
+++ frontend/src/pages/staffApplicationFormPage.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import SAform from '../component/staffApplicationForm'
+import NavbarSmallLight from '../component/navbar-small-light';
 
 export default function staffApplicationForm() {
     return (
         <>
+            <NavbarSmallLight />
+            <section className="py-26 lg:py-36 w-full relative table bg-top bg-no-repeat" id="home">
+                <div className="absolute inset-0 bg-gradient-to-t to-slate-950/50 via-slate-950/75 from-slate-950"></div>
+                <div className="container">
+                    <div className="grid grid-cols-1 pb-8 text-center mt-10">
+                        <h3 className="font-medium leading-normal text-4xl mb-5 mt-10 text-white">Application Form</h3>
+                    </div>
+                </div>
+            </section>
             <div>
                 <SAform />
             </div>
