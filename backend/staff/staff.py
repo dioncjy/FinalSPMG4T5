308e4ffb3ecead82029a50f8ca1fdf045723b43a
dioncheng <dioncheng98@gmail.com>
2023-10-06 00:24:49 +0800

    SPM-76 finished developing BE for auto population of fields for adding a role

--- backend/staff/staff.py
+++ backend/staff/staff.py
@@ -45,6 +45,41 @@ def getAllRoles():
         if connection:
             connection.close()
 
+# Show all unique roles
+def getAllUniqueRoles():
+    try:
+
+        connection, cursor = connect_to_database()
+
+        if connection is None or cursor is None:
+            return jsonify({'error': 'Database connection error'})
+
+        # cursor.execute('SELECT * FROM role GROUP BY "role_name"')
+        cursor.execute('SELECT distinct role_name from role')
+
+        # Fetch all rows from the query result
+        data = cursor.fetchall()
+
+        # Convert the data to a list of dictionaries for JSON response
+        result = []
+        for row in data:
+            result.append({
+                'role_name': row[0]
+                # Add more columns as needed
+            })
+
+        return jsonify(result)
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+    finally:
+        # Close the cursor and database connection
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
 # get specific role name and desc
 def getRoleDetails(role_name):
     try:
@@ -172,51 +207,11 @@ def getIndividualRoleListing(listing_id, role_name):
         role_details = {
             'listing_id': row[0],
             'role_name': row[1],
-            'dpt': row[2],
+            'department': row[2],
             'closing_date': row[3],
             'opening_date': row[4],
-            'reporting_manager': row[5]
-            # Add more columns as needed
-        }
-
-        return jsonify(role_details)
-
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
-    finally:
-        # Close the cursor and database connection
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
-
-# get individual role listing
-def getIndividualRoleListing(listing_id, role_name):
-    try:
-
-        connection, cursor = connect_to_database()
-
-        if connection is None or cursor is None:
-            return jsonify({'error': 'Database connection error'})
-
-        cursor.execute('SELECT * FROM role_listing WHERE role_name = %s and listing_id = %s', (role_name, listing_id,))
-
-
-        # Fetch the result. Since we're expecting a single row, we can use fetchone()
-        row = cursor.fetchone()
-
-        if not row:
-            return jsonify({'error': 'Role skill not found'})
-
-        # Convert the row to a dictionary for JSON response
-        role_details = {
-            'listing_id': row[0],
-            'role_name': row[1],
-            'dpt': row[2],
-            'closing_date': row[3],
-            'opening_date': row[4],
-            'reporting_manager': row[5]
+            'reporting_manager': row[5],
+            'role_description': row[6]
             # Add more columns as needed
         }
 
