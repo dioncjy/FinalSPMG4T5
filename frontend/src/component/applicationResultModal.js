8b9bacb2aaabb01c5b85a789660cf42512b10bbc
joshuahoe <joshuahoe0131@gmail.com>
2023-10-08 14:10:49 +0800

    SPM-83 edit message in modal and update button design

--- frontend/src/component/applicationResultModal.js
+++ frontend/src/component/applicationResultModal.js
@@ -1,6 +1,7 @@
 // Modal.js
 import React from 'react';
 import Modal from 'react-modal';
+import { Button } from "@material-tailwind/react";
 
 Modal.setAppElement('#root'); // Set the root element for accessibility
 
@@ -11,9 +12,17 @@ function ResultModal({ isOpen, onClose }) {
       onRequestClose={onClose}
       contentLabel="Example Modal"
     >
-      <h2>Modal Content</h2>
-      <p>This is the content of your modal.</p>
-      <button onClick={onClose}>Close</button>
+      <div className="flex flex-col items-center justify-centre h-screen">
+            <div className="flex-grow">
+                <h2>Your Application has been submitted!</h2>
+            </div>
+            <div className="flex-grow">
+                <p>The hiring team will review your application.</p>
+            </div>
+            <div className="flex-grow">
+                <Button className="bg-violet-600" onClick={onClose}>Close</Button>
+            </div>
+        </div>
     </Modal>
   );
 }
