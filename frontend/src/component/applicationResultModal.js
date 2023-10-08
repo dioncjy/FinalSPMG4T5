7490dc630b236bd03edcb9c6638eb8effedf6907
joshuahoe <joshuahoe0131@gmail.com>
2023-10-08 14:34:48 +0800

    SPM-83 add checkmark image

--- frontend/src/component/applicationResultModal.js
+++ frontend/src/component/applicationResultModal.js
@@ -10,16 +10,17 @@ function ResultModal({ isOpen, onClose }) {
     <Modal
       isOpen={isOpen}
       onRequestClose={onClose}
-      contentLabel="Example Modal"
+      contentLabel="Application Result Modal"
     >
-      <div className="flex flex-col items-center justify-centre h-screen">
+      <div className="flex flex-col items-center justify-center">
+            <img src="checkmark.png" alt="checkmark" className="py-8"/>
             <div className="flex-grow">
                 <h2>Your Application has been submitted!</h2>
             </div>
-            <div className="flex-grow">
+            <div className="flex-grow py-8">
                 <p>The hiring team will review your application.</p>
             </div>
-            <div className="flex-grow">
+            <div className="flex-grow pt-8">
                 <Button className="bg-violet-600" onClick={onClose}>Close</Button>
             </div>
         </div>
