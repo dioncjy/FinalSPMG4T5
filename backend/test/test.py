b32c51b082770a50e6d57bfb2db41232ef8cf8c2
matthiasclyw <matthias.lee@workato.com>
2023-10-18 15:21:43 +0800

    UPDATE test file
    UPDATE file

--- backend/test/test.py
+++ backend/test/test.py
@@ -1,22 +1,19 @@
 import requests
 import pytest
 
-
-# Define the base URL for your Flask app
-BASE_URL = 'http://127.0.0.1:5000'  # Update the URL if your app runs on a different URL
+BASE_URL = 'http://127.0.0.1:5000' 
 
 # Test your Flask app's endpoints
 def test_get_all_role_listings():
     response = requests.get(f'{BASE_URL}/role_listing')
     assert response.status_code == 200
-    # Add more assertions to check the response data if needed
+    #Add more more more
 
 def test_get_staff_from_module():
     response = requests.get(f'{BASE_URL}/staff')
     assert response.status_code == 200
-    # Add more assertions to check the response data if needed
+    # Add more more
 
-# Add more test functions for other endpoints
 
 if __name__ == '__main__':
     pytest.main()
