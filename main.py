17855ee0f81ec5414d164fa7ce90705158119139
dioncheng <dioncheng98@gmail.com>
2023-10-31 17:20:10 +0800

    cleaned up main.py

--- main.py
+++ main.py
@@ -53,7 +53,6 @@ def create_app():
 
     #  gets all role skills details
     @app.route('/role_skill', methods=['GET']) 
-    
     def get_all_role_skills_details():
         role_skills_data = hr.getAllRoleSkills()
         return role_skills_data
